e335a0b1b2899cccde3ac428391014393540bfa0
strangethingsarehappening12-sketch <strangethingsarehappening12@gmail.com>
2026-09-11 03:49:02 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -129,7 +129,7 @@ export default function App() {
           <Counter
             label="GOAL"
             value={cookwareConfig.targetMarketCap}
-            formatValue={() => 'Billions Valuation'}
+            formatValue={() => 'COOKBILLIONS'}
             compact
           />
         </div>
